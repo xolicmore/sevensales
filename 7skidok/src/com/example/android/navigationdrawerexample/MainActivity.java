280ef3b54445ecbfc63d33cc9c0eee80000e7251
xolic <andrsereb@ya.ru>
2014-03-23 10:55:37 +0200

    gj2

--- 7skidok/src/com/example/android/navigationdrawerexample/MainActivity.java
+++ 7skidok/src/com/example/android/navigationdrawerexample/MainActivity.java
@@ -174,16 +174,18 @@ public class MainActivity extends Activity {
 
     private void selectItem(int position) {
         // update the main content by replacing fragments
-       
+    	getFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+    	//Toast.makeText(this,String.valueOf(getFragmentManager().getBackStackEntryCount()), Toast.LENGTH_LONG).show();getFragmentManager().getBackStackEntryCount();
+	        
     	if (position!=0){
 	    	Fragment fragment = new PlanetFragment();
 	        Bundle args = new Bundle();
 	        args.putInt(PlanetFragment.ARG_PLANET_NUMBER, position);
 	        fragment.setArguments(args);
 	
-	        FragmentManager fragmentManager = getFragmentManager();
-	        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();
-	
+	        FragmentManager fragmentManager = getFragmentManager();	      
+	        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).commit();	
+	        
 	        // update selected item and title, then close the drawer
 	        mDrawerList.setItemChecked(position, true);
 	        setTitle(mPlanetTitles[position]);
--- 7skidok/src/com/example/android/navigationdrawerexample/SalesFragment.java
+++ 7skidok/src/com/example/android/navigationdrawerexample/SalesFragment.java
@@ -1,17 +1,39 @@
 package com.example.android.navigationdrawerexample;
 
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+
+import com.example.android.navigationdrawerexample.MainActivity.PlanetFragment;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class SalesFragment extends Fragment {
     public static final String ARG_PLANET_NUMBER = "planet_number";
-
+    
+    public ArrayList<Sale> Sales;
+    
     public SalesFragment() {
         // Empty constructor required for fragment subclasses
     }
@@ -19,16 +41,60 @@ public class SalesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.sales, container, false);
+    	
+    	
+    	this.Sales=new ArrayList<Sale>();  
+    	
+    	this.Sales.add(new Sale().Sale_test(1, "mexx" , "desc 1"));   
+    	this.Sales.add(new Sale().Sale_test(2, "ret" , "desc 2"));  
+    	this.Sales.add(new Sale().Sale_test(3, "fff " , "desc 3")); 
+        View v = inflater.inflate(R.layout.sales, container, false);        
+       
         
-//        int i = getArguments().getInt(ARG_PLANET_NUMBER);
-//        String planet = getResources().getStringArray(R.array.planets_array)[i];
-//
-//        int imageId = getResources().getIdentifier(planet.toLowerCase(Locale.getDefault()),
-//                        "drawable", getActivity().getPackageName());
-//        ((ImageView) rootView.findViewById(R.id.image)).setImageResource(imageId);
+        ArrayList<Sale> list =this.Sales; 
+        
+        ArrayList<Map<String, Object>> data=new ArrayList<Map<String, Object>>();
+        Map<String, Object> m;
+        
+        for (int i = 0; i < Sales.size(); i++) {
+            m = new HashMap<String, Object>();
+            m.put("shop_id", Sales.get(i).shop_id);
+            m.put("shop_name", Sales.get(i).shop_name);
+            m.put("description", Sales.get(i).description);
+           // m.put(ATTRIBUTE_NAME_IMAGE, R.drawable.ic_launcher);
+            data.add(m);
+          }
+        
+        
+        String[] from = new String[] { "shop_id" , "shop_name" , "description"  };
+        int[] to = new int[] { R.id.shop_id , R.id.shop_name , R.id.description};
+        
+        SimpleAdapter adapter = new SimpleAdapter(
+        		getActivity().getApplicationContext(),
+        		data, R.layout.list_item, from, to);        
+        
+        ListView listview = (ListView) v.findViewById(R.id.sales_list);
+        listview.setAdapter(adapter);
+        
+        listview.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				// getting values from selected ListItem
+				Fragment fragment = new SaleFragment();
+		        Bundle args = new Bundle();
+		        args.putInt("position", position);
+		        fragment.setArguments(args);
+		
+		        FragmentManager fragmentManager = getFragmentManager();
+		        
+		        fragmentManager.beginTransaction().replace(R.id.content_frame, fragment).addToBackStack(null).commit();
+
+			}
+		});
         
         getActivity().setTitle("Скидки");
-        return rootView;
+        return v;
     }
 }
